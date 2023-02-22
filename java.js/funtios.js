6272bab601efcdd819f100c7f95071f6d9718aec
oscarteodorogonzalez <os_cargonzalez@yahoo.com.ar>
2023-02-21 21:54:53 -0300

    web CV

---
+++ java.js/funtios.js
@@ -0,0 +1,10 @@
+
+document.getElementById("boton").onclick = function () {
+
+    console. log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = 
+    "<h1> Estimados Gracias por darme la oportunidad de aprender a programar </h1>"
+     
+}
+
+
